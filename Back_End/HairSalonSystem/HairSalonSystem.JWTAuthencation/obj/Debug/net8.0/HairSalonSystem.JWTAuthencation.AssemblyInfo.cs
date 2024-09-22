d1728d6ec0c72c8944bc86b74f616350fd7f12e0
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2024-09-22 07:41:42 +0700

    Re-Load

--- Back_End/HairSalonSystem/HairSalonSystem.JWTAuthencation/obj/Debug/net8.0/HairSalonSystem.JWTAuthencation.AssemblyInfo.cs
+++ Back_End/HairSalonSystem/HairSalonSystem.JWTAuthencation/obj/Debug/net8.0/HairSalonSystem.JWTAuthencation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HairSalonSystem.JWTAuthencation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+036677d9b02cf7c6971e839070a360beed7bb24b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d513a5e01cfa0ca043b86df5c782aa9ba17e602")]
 [assembly: System.Reflection.AssemblyProductAttribute("HairSalonSystem.JWTAuthencation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HairSalonSystem.JWTAuthencation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Back_End/HairSalonSystem/HairSalonSystem.Repositories/Interface/IGenericRepositoryFactory.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Bean_Mind_Business.Repository.Interface
-{
-    public interface IGenericRepositoryFactory
-    {
-        IGenericRepository<TEntity> GetRepository<TEntity>() where TEntity : class;
-    }
-}
