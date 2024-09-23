cee727b1c074d3755983ff9600c0bb97df7da205
thucnguyen1234 <hantruong3009@gmail.com>
2024-09-23 20:40:03 +0700

    Loại bỏ các tệp tạm thời và thêm .gitignore

--- Back_End/HairSalonSystem/HairSalonSystem/PayLoads/LoginResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HairSalonSystem.API.PayLoads
-{
-    public class LoginResponse
-    {
-        public string Token { get; set; }
-        public string Email { get; set; }
-        public string RoleName { get; set; }
-    }
-}
