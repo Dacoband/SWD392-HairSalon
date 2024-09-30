f8ed272b8abb3d41a52154af34b7d6445a45677d
thucnguyen1234 <hantruong3009@gmail.com>
2024-09-30 11:09:28 +0700

    add

--- Back_End/HairSalonSystem/HairSalonSystem.DAOs/obj/Debug/net8.0/HairSalonSystem.DAOs.AssemblyInfo.cs
+++ Back_End/HairSalonSystem/HairSalonSystem.DAOs/obj/Debug/net8.0/HairSalonSystem.DAOs.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HairSalonSystem.DAOs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cda5f9783ae2cbbf6eec65501be005198828bb9b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28206e94581ae95b47aae1e2ac4ec99d24c4e0e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("HairSalonSystem.DAOs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HairSalonSystem.DAOs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
