8cb2671b12a9d417d1615c036b01495d2781df91
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2024-09-30 11:46:32 +0700

    lib

--- Back_End/HairSalonSystem/HairSalonSystem.DAOs/obj/Debug/net8.0/HairSalonSystem.DAOs.AssemblyInfo.cs
+++ Back_End/HairSalonSystem/HairSalonSystem.DAOs/obj/Debug/net8.0/HairSalonSystem.DAOs.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HairSalonSystem.DAOs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed2327e5507a137ac2cbb908f85afd73adf18ba4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15bb536867296aef7aa26628282b0a786ed86afd")]
 [assembly: System.Reflection.AssemblyProductAttribute("HairSalonSystem.DAOs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HairSalonSystem.DAOs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
