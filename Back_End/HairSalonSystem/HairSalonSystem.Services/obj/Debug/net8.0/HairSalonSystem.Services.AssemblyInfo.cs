5ad0c3e51fec6d2e928b9197a04e17364fd2a272
thucnguyen1234 <hantruong3009@gmail.com>
2024-09-30 11:13:33 +0700

    add12

--- Back_End/HairSalonSystem/HairSalonSystem.Services/obj/Debug/net8.0/HairSalonSystem.Services.AssemblyInfo.cs
+++ Back_End/HairSalonSystem/HairSalonSystem.Services/obj/Debug/net8.0/HairSalonSystem.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HairSalonSystem.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92c2b21ea6eaabba5248aa05f62847b2bd4c5f13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7935797f1725c777d3639015b685209f24eac5d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("HairSalonSystem.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HairSalonSystem.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
