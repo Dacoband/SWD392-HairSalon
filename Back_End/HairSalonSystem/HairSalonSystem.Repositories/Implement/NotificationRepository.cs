308fdfe8b74e7b10f894c79bf6dd2fc0a0535305
thucnguyen1234 <hantruong3009@gmail.com>
2024-09-30 11:40:09 +0700

    fixes notification

--- Back_End/HairSalonSystem/HairSalonSystem.Repositories/Implement/NotificationRepository.cs
+++ Back_End/HairSalonSystem/HairSalonSystem.Repositories/Implement/NotificationRepository.cs
@@ -5,35 +5,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HairSalonSystem.DAOs.Interfaces;
 
 namespace HairSalonSystem.Repositories.Implement
 {
     public class NotificationRepository : INotificationRepository
     {
-        private readonly INotificationRepository _notificationRepository;
-        public NotificationRepository(INotificationRepository notificationRepository)
+        private readonly INotificationDAO _notificationDao;
+        public NotificationRepository(INotificationDAO notificationDao)
         {
-            _notificationRepository = notificationRepository;
+            _notificationDao = notificationDao;
         }
         public async Task CreateNotification(Notifications notification)
         {
-            await _notificationRepository.CreateNotification(notification);
+            await _notificationDao.CreateNotification(notification);
         }
         public async Task DeleteNotification(Guid id)
         {
-            await _notificationRepository.DeleteNotification(id);
+            await _notificationDao.DeleteNotification(id);
         }
         public async Task<Notifications> GetNotificationById(Guid id)
         {
-            return await _notificationRepository.GetNotificationById(id);
+            return await _notificationDao.GetNotificationById(id);
         }
         public async Task<List<Notifications>> GetAllNotifications()
         {
-            return await _notificationRepository.GetAllNotifications();
+            return await _notificationDao.GetAllNotifications();
         }
         public async Task UpdateNotification(Guid id, Notifications notification)
         {
-            await _notificationRepository.UpdateNotification(id, notification);
+            await _notificationDao.UpdateNotification(id, notification);
         }
     }
 }
