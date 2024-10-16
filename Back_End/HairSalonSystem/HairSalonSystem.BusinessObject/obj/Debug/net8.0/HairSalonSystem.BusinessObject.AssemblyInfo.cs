1c86d98a1d63a61dbdaffdcf726873ed91a2aede
thucnguyen1234 <hantruong3009@gmail.com>
2024-10-16 13:15:34 +0700

    Commit

--- Back_End/HairSalonSystem/HairSalonSystem.BusinessObject/obj/Debug/net8.0/HairSalonSystem.BusinessObject.AssemblyInfo.cs
+++ Back_End/HairSalonSystem/HairSalonSystem.BusinessObject/obj/Debug/net8.0/HairSalonSystem.BusinessObject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HairSalonSystem.BusinessObject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51de1186d2bc5899ac08e580af97ba8504293158")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+235099a483862e82821a7a810eeba1f34b6b6f11")]
 [assembly: System.Reflection.AssemblyProductAttribute("HairSalonSystem.BusinessObject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HairSalonSystem.BusinessObject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
