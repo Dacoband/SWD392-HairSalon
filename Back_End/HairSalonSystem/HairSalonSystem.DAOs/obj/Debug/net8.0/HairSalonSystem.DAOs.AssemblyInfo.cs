6d536940ab1d6768cd1e0927b6e066939183f8aa
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2024-10-19 13:00:46 +0700

    lib
    Co-Authored-By: ntmanh18 <141035032+ntmanh18@users.noreply.github.com>
    Co-Authored-By: UserMaster <127202577+thucnguyen12356666@users.noreply.github.com>

--- Back_End/HairSalonSystem/HairSalonSystem.DAOs/obj/Debug/net8.0/HairSalonSystem.DAOs.AssemblyInfo.cs
+++ Back_End/HairSalonSystem/HairSalonSystem.DAOs/obj/Debug/net8.0/HairSalonSystem.DAOs.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HairSalonSystem.DAOs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+262b4700b52cffff7700a264ce94aaf130d1a159")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f95982ad1a0780c0a6c5d9dc13abd75da39e1458")]
 [assembly: System.Reflection.AssemblyProductAttribute("HairSalonSystem.DAOs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HairSalonSystem.DAOs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
