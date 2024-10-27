cf419d2d166002029c6fecfb8469baaad4f11afc
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2024-10-27 20:56:36 +0700

    Fix Member
    1. Chane TimeUtil => DateTime.Now

--- Back_End/HairSalonSystem/HairSalonSystem/Controllers/MemberController.cs
+++ Back_End/HairSalonSystem/HairSalonSystem/Controllers/MemberController.cs
@@ -86,8 +86,8 @@ namespace HairSalonSystem.Services.Controllers
                 Email = memberRequest.Email,
                 Password = PasswordUtil.HashPassword(memberRequest.Password),
                 RoleName = Enums.RoleEnums.MB.GetDescriptionFromEnum(),
-                InsDate = TimeUtils.GetCurrentSEATime(),
-                UpdDate = TimeUtils.GetCurrentSEATime(),
+                InsDate = DateTime.Now,
+                UpdDate = DateTime.Now,
                 DelFlg = true
             };
             await _accountService.AddAccount(account);
@@ -100,8 +100,8 @@ namespace HairSalonSystem.Services.Controllers
                 PhoneNumber = memberRequest.PhoneNumber,
                 Address = memberRequest.Address,
                 AvatarImage = avatarImagePath,
-                InsDate = TimeUtils.GetCurrentSEATime(),
-                UpdDate = TimeUtils.GetCurrentSEATime(),
+                InsDate = DateTime.Now,
+                UpdDate = DateTime.Now,
                 DelFlg = true
             };
 
