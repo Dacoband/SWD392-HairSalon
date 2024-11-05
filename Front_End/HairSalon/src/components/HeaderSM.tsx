234e68f18a839a30ec48318a99726cd58055e04a
TAT610 <trananhtuyet0610@gmail.com>
2024-11-05 07:00:15 +0700

    fix

--- Front_End/HairSalon/src/components/HeaderSM.tsx
+++ Front_End/HairSalon/src/components/HeaderSM.tsx
@@ -6,7 +6,6 @@ import { UserOutlined } from "@ant-design/icons";
 
 const HeaderSM: React.FC = () => {
   const navigate = useNavigate();
-  const isLoggedIn = localStorage.getItem("token"); // Check if user is logged in
 
   const menu = (
     <Menu>
@@ -30,7 +29,7 @@ const HeaderSM: React.FC = () => {
     <div className="bg-white shadow-md h-16 flex items-center">
       <Row align="middle" className="w-full">
         <Col span={4} className="flex items-center">
-          <a onClick={() => navigate("/homePage")}>
+          <a>
             <img
               src="src\assets\logo_mini-removebg-preview.png" // Make sure the path is correct
               className="h-12 ml-16" // Adjust height for a smaller header
@@ -40,28 +39,18 @@ const HeaderSM: React.FC = () => {
         </Col>
         <Col span={20}>
           <div className="flex justify-end items-center pr-12 h-full">
-            {isLoggedIn ? ( // Show the dropdown if the user is logged in
-              <Dropdown
-                overlay={menu}
-                trigger={["click"]}
-                placement="bottomRight"
-              >
-                <a
-                  className="flex hover:text-[#8e7424] items-center cursor-pointer ml-2"
-                  onClick={(e) => e.preventDefault()}
-                >
-                  <UserOutlined style={{ fontSize: "24px" }} />
-                </a>
-              </Dropdown>
-            ) : (
-              // Show login link if the user is not logged in
+            <Dropdown
+              overlay={menu}
+              trigger={["click"]}
+              placement="bottomRight"
+            >
               <a
-                onClick={() => navigate("/login")}
-                className="font-bold px-6 py-2 bg-[#000] hover:text-[#ffff] text-white rounded-full hover:bg-[#74601d]"
+                className="flex hover:text-[#8e7424] items-center cursor-pointer ml-2"
+                onClick={(e) => e.preventDefault()}
               >
-                Login
+                <UserOutlined style={{ fontSize: "24px" }} />
               </a>
-            )}
+            </Dropdown>
           </div>
         </Col>
       </Row>
