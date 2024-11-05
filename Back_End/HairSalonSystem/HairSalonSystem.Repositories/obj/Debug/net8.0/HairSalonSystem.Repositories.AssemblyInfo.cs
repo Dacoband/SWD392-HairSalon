d4109593af6c1a216b8d5a17c77f745f63759bed
thucnguyen1234 <hantruong3009@gmail.com>
2024-11-05 14:43:40 +0700

    commit

--- Back_End/HairSalonSystem/HairSalonSystem.Repositories/obj/Debug/net8.0/HairSalonSystem.Repositories.AssemblyInfo.cs
+++ Back_End/HairSalonSystem/HairSalonSystem.Repositories/obj/Debug/net8.0/HairSalonSystem.Repositories.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HairSalonSystem.Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c01e565fb314aa19bfb6691c9fb766d4bbdfdee7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30ee1cb539b13623b42ba299059f508365e663f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("HairSalonSystem.Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HairSalonSystem.Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
