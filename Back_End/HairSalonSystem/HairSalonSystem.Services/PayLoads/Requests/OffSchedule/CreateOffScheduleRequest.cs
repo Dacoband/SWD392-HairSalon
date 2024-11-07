7adc6520c97915e78b3b4ef2076392a22d7b086e
ntmanh18 <141035032+ntmanh18@users.noreply.github.com>
2024-11-07 22:29:11 +0700

    CreateOffSchedule - Service

---
+++ Back_End/HairSalonSystem/HairSalonSystem.Services/PayLoads/Requests/OffSchedule/CreateOffScheduleRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HairSalonSystem.Services.PayLoads.Requests.OffSchedule
+{
+    public class CreateOffScheduleRequest
+    {
+        public DateOnly OffDate {  get; set; }
+        public int OffSlot { get; set; }    
+    }
+}
