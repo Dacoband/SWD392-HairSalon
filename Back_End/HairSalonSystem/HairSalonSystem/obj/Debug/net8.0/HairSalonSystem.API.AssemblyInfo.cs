f73e5caafd18d27867c4d0c3e2acf71df1e51f6b
thucnguyen1234 <hantruong3009@gmail.com>
2024-11-08 20:02:51 +0700

    fix síu

--- Back_End/HairSalonSystem/HairSalonSystem/obj/Debug/net8.0/HairSalonSystem.API.AssemblyInfo.cs
+++ Back_End/HairSalonSystem/HairSalonSystem/obj/Debug/net8.0/HairSalonSystem.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HairSalonSystem.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8ee01daf538a4c90ce1bb2cfc6aec5efb314298")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e21a248bb5baa0c303667984780908e2dfc42ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("HairSalonSystem.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HairSalonSystem.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
