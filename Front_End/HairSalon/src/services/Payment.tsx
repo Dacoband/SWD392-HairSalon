ec2929eeef764796508a598f5c6392dd1d762982
ntmanh18 <141035032+ntmanh18@users.noreply.github.com>
2024-11-09 14:27:43 +0700

    fix payment ui

--- Front_End/HairSalon/src/services/Payment.tsx
+++ Front_End/HairSalon/src/services/Payment.tsx
@@ -22,3 +22,46 @@ export const createPayment = async (
     throw error
   }
 }
+
+export const successPayment = async (
+  appointmentId: string,
+  status: string
+): Promise<CreatePaymentReponse> => {
+  try {
+    const response = await axios.get<any>(`${API_PAYMENT}/success`, {
+      params: { id: appointmentId, status },
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+        'Content-Type': 'application/json',
+      },
+    })
+
+    return response.data
+  } catch (error) {
+    console.error('Error creating payment link:', error)
+    throw error
+  }
+}
+
+export const cancelPayment = async (
+  appointmentId: string,
+  status: string,
+  code: string,
+  cancel: string,
+  orderCode: string
+): Promise<any> => {
+  try {
+    const response = await axios.get<any>(`${API_PAYMENT}/cancel`, {
+      params: { id: appointmentId, status, code, cancel, orderCode },
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+        'Content-Type': 'application/json',
+      },
+    })
+
+    return response.data
+  } catch (error) {
+    console.error('Error canceling payment:', error)
+    throw error
+  }
+}
